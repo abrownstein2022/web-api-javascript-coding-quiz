120767392b5fdc1accf04d85ca87006843661179
Alexis Brownstein <alexis@drdatabase.com>
2022-10-08 14:05:30 -0400

    question setup in js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,58 @@
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "Commonly Used data types DO not include:",
+    a: [{ text: "1. strings", isCorrect: false },
+        { text: "2. booleans", isCorrect: false },
+        { text: "3. alerts", isCorrect: true },
+        { text: "4. numbers", isCorrect: false }
+    ]
+
+},
+{
+    id: 1,
+    q: "The condition in an if / else statement is enclosed with __________.",
+    a: [{ text: "1. quotes", isCorrect: false },
+        { text: "2. curly brackets", isCorrect: false },
+        { text: "3. parentheses", isCorrect: true },
+        { text: "4. square brackets", isCorrect: false }
+    ]
+
+},
+{
+    id: 2,
+    q: "Arrays in JavaScript can be used to store ____________.",
+    a: [{ text: "numbers and strings", isCorrect: false },
+        { text: "other arrays", isCorrect: false },
+        { text: "booleans", isCorrect: false },
+        { text: "all of the above", isCorrect: true }
+    ]
+
+}
+,
+{
+    id: 3,
+    q: "String values must be enclosed within ____________ when being assigned to variables.",
+    a: [{ text: "commas", isCorrect: false },
+        { text: "curly brackets", isCorrect: false },
+        { text: "quotes", isCorrect: true },
+        { text: "parentheses", isCorrect: false }
+    ]
+
+}
+,
+{
+    id: 4,
+    q: "A very useful tool used during development and debugging for printing content to the debugger is ____________.",
+    a: [{ text: "JavaScript", isCorrect: false },
+        { text: "terminal/bash", isCorrect: false },
+        { text: "for loops", isCorrect: false },
+        { text: "console log", isCorrect: true }
+    ]
+
+}
+]
+
 var wordBlank = document.querySelector(".word-blanks");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
